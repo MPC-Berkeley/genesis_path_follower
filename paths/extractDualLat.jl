6a2458e16f710dc41d40ff67c2e324f29ea2db51
GXZ <xiaojing.zhang@berkeley.edu>
2018-10-24 12:57:25 -0700

    added code to extract dual for LAT control

--- paths/extractDualLat.jl
+++ paths/extractDualLat.jl
@@ -27,15 +27,15 @@ latData = matread("NN_test_trainingData.mat")
 
 # inputParam_lat = np.hstack((ey_curr.T, epsi_curr.T ,df_prev.T, v_pred, c_pred))
 inputParam_lat = latData["inputParam_lat"]   #
-outputParamAcc_long = latData["outputParamAcc_long"]
-outputParamDacc_long = latData["outputParamDacc_long"]
+outputParamDf_lat = latData["outputParamDf_lat"]
+outputParamDdf_lat = latData["outputParamDdf_lat"]
 
 # parse initial data
-s_curr_all = inputParam_long[:,1]
-v_curr_all = inputParam_long[:,2]
-a_prev_all = inputParam_long[:,3]
-s_ref_all = inputParam_long[:,4:4+N-1]
-v_ref_all = inputParam_long[:,12:end]
+ey_curr_all = inputParam_lat[:,1]
+epsi_curr_all = inputParam_lat[:,2]
+df_prev_all = inputParam_lat[:,3]
+v_pred_all = inputParam_lat[:,4:4+N-1]
+c_pred_all = inputParam_lat[:,12:end]
 
 
 u_ref_init = kmpcLinLat.u_ref_init	# if not used, set cost to zeros
@@ -46,9 +46,9 @@ u_ref_init = kmpcLinLat.u_ref_init	# if not used, set cost to zeros
 # x_tilde_k := (x_k , u_{k-1})
 # u_tilde_k := (u_k - u_{k-1})
 
-A_tilde = kmpcLinLat.A_tilde
+# A_tilde = kmpcLinLat.A_tilde
 
-g_tilde = kmpcLinLat.g_tilde
+# g_tilde = kmpcLinLat.g_tilde
 
 # # ================== Transformation 2 ======================
 # # bring into GUROBI format
@@ -63,7 +63,7 @@ n_uxu = kmpcLinLat.n_uxu	# size of one block of (u_tilde, x_tilde) = (deltaU, x,
 # Build cost function
 # cost for (u_tilde, x_tilde) = (deltaU , S, V, U)
 H_gurobi = kmpcLinLat.H_gurobi
-
+f_gurobi_init = kmpcLinLat.f_gurobi_init
 
 # build box constraints lb_gurobi <= z <= ub_gurobi
 # recall: z = (u_tilde, x_tilde, ....)
@@ -74,63 +74,77 @@ ub_gurobi = kmpcLinLat.ub_gurobi		# (deltaU, X, U)
 # build equality matrix (most MALAKA task ever)
 nu_tilde = kmpcLinLat.nu_tilde
 nx_tilde = kmpcLinLat.nx_tilde
-Aeq_gurobi = kmpcLinLat.Aeq_gurobi
+# Aeq_gurobi = kmpcLinLat.Aeq_gurobi
 
 
 ######################## ITERATE OVER saved parameters ################
 # build problem
-num_DataPoints = length(s_curr_all)
+num_DataPoints = length(ey_curr_all)
 solv_time_all = zeros(num_DataPoints)
-a_res_all = zeros(num_DataPoints)
-dA_res_all = zeros(num_DataPoints)
+df_res_all = zeros(num_DataPoints)
+ddf_res_all = zeros(num_DataPoints)
 
-outputParamDualEQ_long = zeros(num_DataPoints, N*(nx+nu))
-outputParamDualLB_long = zeros(num_DataPoints, N*n_uxu)
-outputParamDualUB_long = zeros(num_DataPoints, N*n_uxu)
+outputParamDualEQ_lat = zeros(num_DataPoints, N*(nx+nu))
+outputParamDualLB_lat = zeros(num_DataPoints, N*n_uxu)
+outputParamDualUB_lat = zeros(num_DataPoints, N*n_uxu)
 
 dual_eq = []
 dual_ub = []
 dual_lb = []
 
-for ii = 1 : 1	
+for ii = 1 : num_DataPoints	
 	# extract appropriate parameters
-	s_0 = s_curr_all[ii]
-	v_0 = v_curr_all[ii]
-	u_0 = a_prev_all[ii]
-	s_ref = s_ref_all[ii,:]
-	v_ref = v_ref_all[ii,:]
-	acc_stored = outputParamAcc_long[ii,:]
-	dAcc_stored = outputParamDacc_long[ii,:]
+	ey_0 = ey_curr_all[ii]
+	epsi_0 = epsi_curr_all[ii]
+	u_0 = df_prev_all[ii]
+	v_pred = v_pred_all[ii,:]
+	c_pred = c_pred_all[ii,:]
+	df_stored = outputParamDf_lat[ii,:]
+	ddf_stored = outputParamDdf_lat[ii,:]
 
-
-	x0 = [s_0 ; v_0]
+	# build problem (only the updated parts)
+	x0 = [ey_0 ; epsi_0]
 	u0 = u_0 				# it's really u_{-1}
 	x_tilde_0 = [x0 ; u0]	# initial state of system; PARAMETER
 
-	# update RHS of linear equality constraint
-	beq_gurobi_updated = repmat(g_tilde,N,1);
-	beq_gurobi_updated[1:nx_tilde] = beq_gurobi_updated[1:nx_tilde] + A_tilde*x_tilde_0 	# PARAMETER: depends on x0
-
-	# update reference trajectories
-	x_ref = zeros(N*nx,1)
+	# system dynamics A, B, c defined later on (they depend on reference)
+	A_updated = zeros(nx, nx, N)
+	B_updated = zeros(nx, nu, N)
+	g_updated = zeros(nx, N)
 	for i = 1 : N
-		x_ref[(i-1)*nx+1] = s_ref[i]		# set x_ref, s_ref/v_ref is of dim N+1; index of s_ref changed
-		x_ref[(i-1)*nx+2] = v_ref[i]		# set v_ref
+		A_updated[:,:,i] = [	1	dt*v_pred[i] 
+								0		1			]
+		B_updated[:,:,i] = [	dt*v_pred[i]*L_b/(L_a+L_b) 
+								dt*v_pred[i]/(L_a + L_b)	]
+		g_updated[:,i] = [ 0	# column vector
+						-dt*v_pred[i]*c_pred[i] 	]
 	end
-	# augment state with input for deltaU-formulation
-	x_tilde_ref = zeros(N*(nx+nu))
-	for i = 1 : N
-		x_tilde_ref[(i-1)*(nx+nu)+1 : (i-1)*(nx+nu)+nx] = x_ref[(i-1)*nx+1 : (i-1)*nx+nx]
-		x_tilde_ref[(i-1)*(nx+nu)+nx+1 : (i-1)*(nx+nu)+nx+nu] = u_ref_init[i]	# u_ref_init always 0, but no no weights
+	
+	# x_tilde transformation
+	# update system matrices for tilde-notation
+	A_tilde_updated = zeros(nx+nu,nx+nu,N)
+	B_tilde_updated = zeros(nx+nu,nu,N)
+	g_tilde_updated = zeros(nx+nu,N)
+	for i = 1 : N 
+		A_tilde_updated[:,:,i] = [ 	A_updated[:,:,i]  		B_updated[:,:,i] 
+									zeros(nu,nx)   			eye(nu)			]
+		B_tilde_updated[:,:,i] = [	B_updated[:,:,i] 	;  	eye(nu)	]
+		g_tilde_updated[:,i] =   [	g_updated[:,i]		; 	zeros(nu) ]
 	end
-	u_tilde_ref = zeros(N*nu) 	# want to minimize deltaU = u_k - u_{k-1}
 
-	z_gurobi_ref = zeros(N*n_uxu) 	# reference point for z_gurobi ; PARAMETER!
+	# z-transformation
+	Aeq_gurobi_updated = zeros(N*nx_tilde , N*(nx_tilde+nu_tilde))
+	Aeq_gurobi_updated[1:nx_tilde, 1:(nx_tilde+nu_tilde)] = [-B_tilde_updated[:,:,1] eye(nx_tilde)] 	# fill out first row associated with x_tilde_1
+	for i = 2 : N  	# fill out rows associated to x_tilde_2, ... , x_tilde_N
+		Aeq_gurobi_updated[ (i-1)*nx_tilde+1 : i*nx_tilde  , (i-2)*(nu_tilde+nx_tilde)+(nu_tilde)+1 : (i-2)*(nu_tilde+nx_tilde)+nu_tilde+(nx_tilde+nu_tilde+nx_tilde)    ] = [-A_tilde_updated[:,:,i] -B_tilde_updated[:,:,i] eye(nx_tilde)]
+	end
+
+	# right-hand-size of equality constraint
+	beq_gurobi_updated = zeros(N*nx_tilde)
 	for i = 1 : N
-		z_gurobi_ref[(i-1)*n_uxu+1 : (i-1)*n_uxu+nu] = u_tilde_ref[(i-1)*nu+1 : i*nu] 		# should be zero for this application
-		z_gurobi_ref[(i-1)*n_uxu+nu+1 : i*n_uxu] = x_tilde_ref[(i-1)*(nx+nu)+1 : i*(nu+nx)]
-	end 
-	f_gurobi_updated = -2*H_gurobi*z_gurobi_ref
+		beq_gurobi_updated[(i-1)*nx_tilde+1:i*nx_tilde] = g_tilde_updated[:,i]
+	end
+	beq_gurobi_updated[1:nx_tilde] = beq_gurobi_updated[1:nx_tilde] + A_tilde_updated[:,:,1]*x_tilde_0 	# PARAMETER: depends on x0
 
 
 
@@ -143,10 +157,10 @@ for ii = 1 : 1
 	#				z_lb <= z <= z_ub
 	mdl = Model(solver=GurobiSolver(Presolve=0, LogToConsole=0))
 	@variable(mdl, z[1:N*n_uxu])  	# decision variable; contains everything
-	@objective(mdl, Min, z'*H_gurobi*z + f_gurobi_updated'*z)
-	constr_eq = @constraint(mdl, Aeq_gurobi*z .== squeeze(beq_gurobi_updated,2))
-	constr_ub = @constraint(mdl, z .<= squeeze(ub_gurobi,2))
-	constr_lb = @constraint(mdl, -z .<= -squeeze(lb_gurobi,2))
+	@objective(mdl, Min, z'*H_gurobi*z + f_gurobi_init'*z)
+	constr_eq = @constraint(mdl, Aeq_gurobi_updated*z .== beq_gurobi_updated)
+	constr_ub = @constraint(mdl, z .<= ub_gurobi)
+	constr_lb = @constraint(mdl, -z .<= -lb_gurobi)
 
 	tic()
 	status = solve(mdl)
@@ -154,40 +168,41 @@ for ii = 1 : 1
 
 	# extract solution
 	z_opt = getvalue(z)
-	# structure of z = [ (dAcc,s,v,Acc) ; (dAcc, s, v, Acc) ; ... ]
-	dA_pred_opt = z_opt[1:n_uxu:end]
-	a_pred_opt = z_opt[4:n_uxu:end]
-	s_pred_opt = z_opt[2:n_uxu:end]  	# does not include s0
-	v_pred_opt = z_opt[3:n_uxu:end] 		# does not include v0 
+		# structure of z = [ (ddf,ey,epsi,df) ; (ddf, ey, epsi, df) ; ... ]
+	ddf_pred_opt = z_opt[1:n_uxu:end]
+	df_pred_opt = z_opt[4:n_uxu:end]
+	ey_pred_opt = z_opt[2:n_uxu:end]  	# does not include s0
+	epsi_pred_opt = z_opt[3:n_uxu:end] 		# does not include v0 
 
 	#### compare solution ####
-	a_res_all[ii] = norm(a_pred_opt - acc_stored)
-	dA_res_all[ii] = norm(dA_pred_opt - dAcc_stored)
+	df_res_all[ii] = norm(df_pred_opt - df_stored)
+	ddf_res_all[ii] = norm(ddf_pred_opt - ddf_stored)
 
 	#### extract dual variables ####
 	dual_eq = getdual(constr_eq)
 	dual_ub = getdual(constr_ub)
 	dual_lb = getdual(constr_lb)
 
-	outputParamDualEQ_long[ii,:] = dual_eq
-	outputParamDualLB_long[ii,:] = dual_ub
-	outputParamDualUB_long[ii,:] = dual_ub
+	outputParamDualEQ_lat[ii,:] = dual_eq
+	outputParamDualLB_lat[ii,:] = dual_ub
+	outputParamDualUB_lat[ii,:] = dual_ub
 
 end
 
-println("max a-residual:  $(maximum(a_res_all))")
-println("max dA-residual:  $(maximum(dA_res_all))")
-println("max solv-time (excl modelling time):  $(maximum(solv_time_all[3:end]))")
-println("avg solv-time (excl modelling time):  $(mean(solv_time_all[3:end]))")
+println("max df-residual:  $(maximum(df_res_all))")
+println("max ddf-residual:  $(maximum(ddf_res_all))")
+println("max solv-time (excl modelling time):  $(maximum(solv_time_all[2:end]))")
+println("avg solv-time (excl modelling time):  $(mean(solv_time_all[2:end]))")
 
 matwrite("NN_test_trainingDataLat_PrimalDual.mat", Dict(
-	"inputParam_long" => inputParam_long,
-	"outputParamAcc_long" => outputParamAcc_long,
-	"outputParamDacc_long" => outputParamDacc_long,
-	"outputParamDualEQ_long" => outputParamDualEQ_long,
-	"outputParamDualLB_long" => outputParamDualLB_long,
-	"outputParamDualUB_long" => outputParamDualUB_long
+	"inputParam_lat" => inputParam_lat,
+	"outputParamDf_lat" => outputParamDf_lat,
+	"outputParamDdf_lat" => outputParamDdf_lat,
+	"outputParamDualEQ_lat" => outputParamDualEQ_lat,
+	"outputParamDualLB_lat" => outputParamDualLB_lat,
+	"outputParamDualUB_lat" => outputParamDualUB_lat
 ))
 
 
-println("---- done extracting and saving dual for LONG control ----")
+
+println("---- done extracting and saving dual for LAT control ----")
