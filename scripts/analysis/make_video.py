fd72190e3bb1f3a33b1459ada993235584cf4d37
Vijay Govindarajan <govvijay@berkeley.edu>
2018-12-16 13:55:26 -0800

    Adding explicit MPC cost plotting code (for George/Monimoy).

--- scripts/analysis/make_video.py
+++ scripts/analysis/make_video.py
@@ -2,18 +2,21 @@
 import argparse
 from anim_utils.anim_track import AnimateTrack
 from anim_utils.anim_vehicle import AnimateVehicle
+from anim_utils.anim_empc import AnimateEMPC
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser('Convert bag file and reference mat file to a video.')
 	parser.add_argument('--bag', type=str, required=True, help='Bag file to animate.')
 	parser.add_argument('--mat', type=str, required=True, help='Matfile with the reference path.')
 	parser.add_argument('--vid', type=str, required=True, help='Video output filename (mp4).')
-	parser.add_argument('--type', type=str, choices=['track', 'vehicle'], help='Plot full track or zoomed-in view of vehicle.')
+	parser.add_argument('--type', type=str, choices=['track', 'vehicle', 'empc'], help='Plot full track or zoomed-in view of vehicle.')
 	args = parser.parse_args()
 
 	if args.type == 'track':
 		AnimateTrack(args.bag, args.mat, args.vid)
 	elif args.type == 'vehicle':
 		AnimateVehicle(args.bag, args.mat, args.vid)
+	elif args.type == 'empc':
+		AnimateEMPC(args.bag, args.mat, args.vid)
 	else:
 		raise ValueError("Invalid video type!  Accepted types are <track> or <vehicle>.")
