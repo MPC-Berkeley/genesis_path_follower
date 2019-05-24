c7d937c08bd3df60291e1a93d04174e14480c3e3
Nitin <nkapania@gmail.com>
2019-05-24 13:32:05 -0700

    using a class for lidarboxsort

--- scripts/lidarboxsort_nk.py
+++ scripts/lidarboxsort_nk.py
@@ -1,41 +1,68 @@
+#!/usr/bin/env python
 import rospy
 from jsk_recognition_msgs.msg import BoundingBoxArray
 from jsk_recognition_msgs.msg import BoundingBox
 from genesis_path_follower.msg import cur_state
+from genesis_path_follower.msg import ped_state
 import numpy as np
 
-def parseCurState(data):
-	print(data.distance_m)
+#This class takes in bounding boxes from Euclidean clustering of LIDAR data
+#and returns the X position of the pedestrian in the crosswalk
+
+class LidarBoxSort():
+
+	def __init__(self):
+		rospy.init_node('lidar_box_filter', anonymous = True)
+		rospy.Subscriber('/bounding_boxes', BoundingBoxArray, self.parseBoxes, queue_size = 2)
+		rospy.Subscriber('/vehicle/cur_state', cur_state, self.parseCurState, queue_size = 2)
+		self.ped_pub = rospy.Publisher("/ped_state", ped_state, queue_size =2)
+		self.dist2crosswalk = 10.0 #initial value, will be updated by parseCurState
+		self.posX = None; #pedestrian X position
+		self.posY = None; #pedestrian Y position 
+		self.yBounds = 5.0; #defines search region for Euclidean clusters
+		self.xBounds = 5.0; #defines search region for Euclidean clusters
+		self.pub_loop()
+
+	def parseCurState(self,data):
+		self.dist2crosswalk = data.distance_m
+
+	def parseBoxes(self,data):
+		x = []
+		y = []
+
+		for i in range(len(data.boxes)):
+			x.append(data.boxes[i].pose.position.x)
+			y.append(data.boxes[i].pose.position.y)
+
+ 		#search for clusters in the crosswalk
+		x = np.array(x); y = np.array(y);
+		assert len(x) == len(y);
+
+		arrY = np.nonzero(np.logical_and(y >= self.dist2crosswalk - self.yBounds, y <= self.dist2crosswalk + self.yBounds))
+		arrX = np.nonzero(np.logical_and(x >= -self.xBounds, x <= self.xBounds))
+
+		possiblePedestrian = np.intersect1d(arrX, arrY)
+		if len(possiblePedestrian) > 1:
+			print("Warning - more than 1 pedestrian found")
+
+		print([x[possiblePedestrian], y[possiblePedestrian]])
+
+	def pub_loop(self):
+		pedestrian_state = ped_state()
+		pedestrian_state.header.stamp = rospy.Time.now()
+
+		pedestrian_state.posX = self.posX
+		pedestrian_state.posY = self.posY
+		self.ped_pub.publish(pedestrian_state)
+		self.r.sleep()
+	
 
 
 
-def parseBoxes(data):
-	x = []
-	y = []
-
-	dist2crosswalk  = 10.0 #harcoded for now
-
-	for i in range(len(data.boxes)):
-		x.append(data.boxes[i].pose.position.x)
-		y.append(data.boxes[i].pose.position.y)
 
 
-	#search for clusters in the crosswalk
-	x = np.array(x); y = np.array(y);
-	arrY = np.nonzero(np.logical_and(y >= dist2crosswalk - 5, y <= dist2crosswalk + 5))
-	arrX = np.nonzero(np.logical_and(x >= -5, x <= 5))
-
-	possiblePedestrian = np.intersect1d(arrX, arrY)
-
-	print([x[possiblePedestrian], y[possiblePedestrian]])
-
-
-
-def lidar_loop():
-	rospy.init_node('lidar_box_filter', anonymous = True)
-	rospy.Subscriber('/bounding_boxes', BoundingBoxArray, parseBoxes, queue_size = 2)
-	#rospy.Subscriber('/vehicle/cur_state', cur_state, parseCurState, queue_size = 2)
-
-	rospy.spin()
-if __name__=='__main__':
-	lidar_loop()
+if __name__=="__main__":
+	try:
+		lbs = LidarBoxSort()
+	except rospy.ROSInterruptException:
+		pass # to handle node shutdown cleanly
